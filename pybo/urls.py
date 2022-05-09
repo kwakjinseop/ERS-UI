0482c53323829ec8807e0b8750b957a2d6fc5538
kwakjinseop <xmcdk@naver.com>
2022-05-09 22:23:53 +0900

    수정

--- pybo/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-
-    path('', views.index, name='index'),
-    path('use/', views.use, name='use'),
-    #path('use/', views.click),
-]
